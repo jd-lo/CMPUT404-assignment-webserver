8109ef1e7ca4f2a8a84b69787f8aaf3955a8cc89
jd-lo <jdlo@ualberta.ca>
2023-01-22 12:53:11 -0800

    Add skeleton for future request handling. Refactor code.

--- requestParser.py
+++ requestParser.py
@@ -1,35 +1,68 @@
-#Takes a request in its byte form and returns a dictionary of its
-#expected key-value pairs.
+from os import path
 
-def parseReqBytes(byteStream: bytearray) :
+#Class of parsed fields from the byte request
+#Contains method to return the appropriate response, complete with header and body.
 
-    tokenStream = byteStream.decode('utf-8')
-    keyValPairs = tokenStream.split('\r\n')
+class ClientRequest:
+    def __init__(self, request: bytearray):
 
-    dictRequest = {}
+        self.valid = False
+        parsedRequest = self._parseReqBytes(request)
+        if parsedRequest:
+            self.valid = True
+            for field, value in parsedRequest.items():
+                self.__setattr__(field, value)
 
-    #The first entry "[REQ] / ... does not follow conventional pattern; deal with seperately"
-    requestProtocolStr = keyValPairs.pop(0)
-    sepEntries = requestProtocolStr.split(" ")
-    dictRequest.update({"Method": sepEntries[0], "Path": sepEntries[1], "Protocol": sepEntries[2]})
-    
-    #The next entry, Host needs to be split into respective host and port
-    hostPortStr = keyValPairs.pop(0)
-    sepEntries = hostPortStr.split(":")
-    dictRequest.update({"Host": sepEntries[1].strip(), "Port": sepEntries[2]})
+    #Converts a request byte sequence into a dictionary, return None if request is not correct format.
+    def _parseReqBytes(self, request: bytearray) :
+        tokenStream = request.decode('utf-8')
+        keyValPairs = tokenStream.split('\r\n')
 
-    #All remaining entries standard.
-    for keyValStr in keyValPairs:
-        keyVal = keyValStr.split(':', maxsplit = 1)
-        dictRequest.update({keyVal[0].strip(): keyVal[1].strip()})
+        dictRequest = {}
 
-    return dictRequest
+        try:
+            #The first entry "[REQ] / ... does not follow conventional pattern; deal with seperately"
+            requestProtocolStr = keyValPairs.pop(0)
+            sepEntries = requestProtocolStr.split(" ")
+            dictRequest.update({"method": sepEntries[0], "path": sepEntries[1].lstrip('/'), "protocol": sepEntries[2]})
+            
+            #The next entry, Host needs to be split into respective host and port
+            hostPortStr = keyValPairs.pop(0)
+            sepEntries = hostPortStr.split(":")
+            dictRequest.update({"host": sepEntries[1].strip(), "port": sepEntries[2]})
 
+            #All remaining entries standard.
+            for keyValStr in keyValPairs:
+                keyVal = keyValStr.split(':', maxsplit = 1)
+                dictRequest.update({keyVal[0].strip().lower(): keyVal[1].strip()})
 
-sampleReq = b'GET / HTTP/1.1\r\nHost: 127.0.0.1:8080\r\nUser-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0\r\nAccept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate, br\r\nConnection: keep-alive\r\nUpgrade-Insecure-Requests: 1\r\nSec-Fetch-Dest: document\r\nSec-Fetch-Mode: navigate\r\nSec-Fetch-Site: none\r\nSec-Fetch-User: ?1'
-sampleReq2 = b'GET /favicon.ico HTTP/1.1\r\nHost: 127.0.0.1:8080\r\nUser-Agent: Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:108.0) Gecko/20100101 Firefox/108.0\r\nAccept: image/avif,image/webp,*/*\r\nAccept-Language: en-US,en;q=0.5\r\nAccept-Encoding: gzip, deflate, br\r\nConnection: keep-alive\r\nReferer: http://127.0.0.1:8080/\r\nSec-Fetch-Dest: image\r\nSec-Fetch-Mode: no-cors\r\nSec-Fetch-Site: same-origin'
-sampleReq3 = b'Hello World' #When we pipe in something using netcat...
-sampleReq4 = b'GET / HTTP/1.1\r\nHost: 127.0.0.1:8080\r\nUser-Agent: curl/7.68.0\r\nAccept: */*'
+            return dictRequest
+        except:
+            return None
 
-myDict = parseReqBytes(sampleReq4)
-    
+    def isValid(self):
+        return self.valid
+
+    def getResponse(self):
+        #Status, MIME types, body
+        if not self.isValid():
+            #Code 400 (Bad Request)
+            pass
+        elif self.getattr(self, 'method') != 'GET':
+            #Code 405 (Method not allowed)
+            pass
+        elif self.getattr(self, 'path') != path.exists():
+            #Code 301 (Redirect, provided corrected path exists)
+            #Code 404 If above fails (Not found otherwise)
+            pass
+        elif self.getattr(self, 'path') != path.isfile():
+            #Code 404 (Not found)
+            pass
+        else:
+            #Code 200 (OK)
+            pass
+
+#For use debugging
+if __name__=='__main__':
+    sampleReq = b'Hello World'
+    test = ClientRequest(sampleReq)
